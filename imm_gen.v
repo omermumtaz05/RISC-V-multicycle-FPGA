49d2796d30d6876b28fdcdc7cf182787c8536e39
omermumtaz05 <omermumtaz05@gmail.com>
2025-07-17 18:01:38 +0400

    Update imm_gen.v
    fixed and verified

--- imm_gen.v
+++ imm_gen.v
@@ -1,27 +1,24 @@
 module imm_gen(
     input [31:0] inst,
-    output [11:0] imm
+  	output reg [31:0] imm
 );
 
 
     always @ (*)
         case(inst[6:0])
 
-            7'b0000011:
-                imm = inst[31:20]; // lw
+            7'b0000011,
+          	7'b0010011:
+              imm = {{20{inst[31]}}, inst[31:20]}; // lw and addi
 
             7'b0100011:
-                imm[11:5] = inst[31:25]; //sw
-                imm[4:0] = inst[11:7];
 
-            7'b0010011:
-                imm[11:5] = inst[31:20]; // addi
+          		imm = {{20{inst[31]}}, inst[31:25], inst[11:7]};
+          		
 
             7'b1100011:
-                imm[12] = inst[31]; // beq
-                imm[11] = inst[7];
-                imm[10:5] = inst[30:25];
-                imm[4:1] = inst[11:8];
+      
+  		 		imm = {{19{inst[31]}}, inst[7], inst[30:25], inst[11:8], 1'b0};
 
         endcase
 
