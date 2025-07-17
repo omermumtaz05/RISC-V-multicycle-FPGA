cb8ed460db5bbf2ee03b44e03ead8828bb42b63a
omermumtaz05 <omermumtaz05@gmail.com>
2025-07-17 18:03:22 +0400

    added reset but not verified

--- PC.v
+++ PC.v
@@ -2,11 +2,14 @@ module ProgramCounter(
   input PCWrite,
   input [31:0] next_pc,
   input clk,
+  input reset,
   output reg [31:0] pc);
   
 
   always @ (posedge clk)
-    if(PCWrite)
+    if(reset)
+      pc <= 32'b0
+    else if(PCWrite)
       pc <= next_pc;
     
   
