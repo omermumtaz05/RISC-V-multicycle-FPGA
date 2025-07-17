ba7d1856546d1c733da6814b4a7a1741b08fc544
omermumtaz05 <omermumtaz05@gmail.com>
2025-07-17 18:05:40 +0400

    added reset and verif A.v

--- A.v
+++ A.v
@@ -1,13 +1,17 @@
 module A(
     input [31:0] readData1,
     input clk,
+    input reset, 
 
-    output [31:0] dataA
+    output reg [31:0] dataA
 
 );
 
 
     always @ (posedge clk)
-        dataA <= readData1;
+        if(reset)
+            dataA <= 32'b0;
+        else
+            dataA <= readData1;
         
 endmodule
