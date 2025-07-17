f8824812b2efef12748cdf120ad36074b1340bc6
omermumtaz05 <omermumtaz05@gmail.com>
2025-07-17 18:04:43 +0400

    added reset & verif ALUOut.v

--- ALUOut.v
+++ ALUOut.v
@@ -1,13 +1,17 @@
 module ALUOut(
     input [31:0] ALUResult,
     input clk,
+    input reset,
 
-    output ALUout
+  	output reg [31:0] ALUOut_result
 );
 
 
     always @ (posedge clk)
-        ALUout <= ALUResult;
+        if(reset)
+            ALUOut_result <= 32'b0;
+        else
+            ALUOut_result <= ALUResult;
 
         
 endmodule
