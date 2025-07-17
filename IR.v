058ebc97a9865451d917feee6badd3bdd5fc26d6
omermumtaz05 <omermumtaz05@gmail.com>
2025-07-17 18:03:46 +0400

    added reset and verified

--- IR.v
+++ IR.v
@@ -2,14 +2,18 @@ module IR(
     input [31:0] MemData,
     input IRWrite,
     input clk,
-
-    output [31:0] inst
+	input reset,
+  
+    output reg [31:0] inst
 
 );
 
     always @ (posedge clk)
-        if(IRWrite)
-            inst <= MemData;
+      if(reset)
+        inst <= 32'b0;
+        
+      else if(IRWrite)
+        inst <= MemData;
 
 
 endmodule 
