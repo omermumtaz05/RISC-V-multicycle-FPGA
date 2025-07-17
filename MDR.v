a8af5ceb667f166959b16a0b44ed8a3560908985
omermumtaz05 <omermumtaz05@gmail.com>
2025-07-17 18:02:13 +0400

    Update MDR.v
    added reset and verified

--- MDR.v
+++ MDR.v
@@ -1,11 +1,17 @@
 module MDR(
-    input [31:0] MemData,
+    input [31:0] memData,
+    input reset,
     input clk,
+    
 
-    output [31:0] data
+  	output reg [31:0] data_out
 );
 
     always @ (posedge clk)
-        data <= MemData;
+      if(reset)
+        data_out <= 32'b0;
+  
+  	  else
+        data_out <= memData;
 
 endmodule
