7c6e5c87bba8982feed7e9ad57fcd848423b0725
Muhammadahmed-1 <68020566+Muhammadahmed-1@users.noreply.github.com>
2025-07-19 03:44:13 -0400

    Update muxes.v

--- muxes.v
+++ muxes.v
@@ -1,54 +1,51 @@
-module addrsel_mux (addrsel,PC,OpB,M1); //2 to 1 mux
-    input  wire addrsel;
+module mem_mux (IorD,PC,OpB,M1); //2 to 1 mux
+    input  wire IorD;
     input  wire [7:0]  PC;     
-    input  wire [7:0]  OpB;    
+    input  wire [7:0]  ALUOut;    
     output wire [7:0]  M1;       
 
-    assign M1 = (addrsel) ? OpB : PC;
+    assign M1 = (IorD) ? ALUOut : PC;
 endmodule
 
-module OpASel_mux (OpASel,IR6_7,M2);
-       input wire OpASel;
-       input wire [1:0] IR6_7;
-       output wire [1:0] M2;
+module A_mux (ALUSrcA,A,PC,M2);
+       input wire ALUSrcA;
+       input wire [7:0] A;
+       input wire [7:0] PC;
+       output wire [7:0] M2;
 
-       assign M2 = (OpASel) ? 2'b01  : IR6_7;
+       assign M2 = (ALUSrcA) ? A  : PC;
 endmodule       
 
-module RegIn_mux (RegIn,MDR,ALUout,M3);
-       input wire RegIn;
+module MDR_mux (MemtoReg,MDR,ALUOut,M3);
+       input wire MemtoReg;
        input wire  [7:0]  MDR;
-       input wire  [7:0] ALUout;
+       input wire  [7:0] ALUOut;
        output wire [7:0] M3;
 
-       assign M3 = (RegIn) ? MDR : ALUout;
+       assign M3 = (MemtoReg) ? MDR : ALUOut;
 
 endmodule
 
-module ALU1_mux(ALU1,PC,OpB,M4);
-       input wire ALU1;
-       input wire [7:0] OpA;
-       input wire [7:0] PC;
+module PC_mux(PCSource,ALUOut,ALUResult,M4);
+       input wire PCSource;
+       input wire [7:0] ALUOut;
+       input wire [7:0] ALUResult;
        output wire [7:0] M4;
 
-        assign M4 = (ALU1) ? OpA : PC;
+        assign M4 = (PCSource) ? ALUOut : ALUResult;
 endmodule       
-// 5 to 1 mux    
-module  ALU2_mux(ALU2,OpB,Imm4, Imm5,Imm3,M5);
-        input  wire [2:0]  ALU2;
-        input  wire [7:0]  OpB;
-        input  wire [7:0]  Imm4;
-        input  wire [7:0]  Imm5;
-        input  wire [7:0]  Imm3;
+// 3 to 1 mux    
+module  B_mux(ALUSrcB,B,Imm3,M5);
+        input  wire  [1:0] ALUSrcB;
+        input  wire [7:0]  B;
+        input  wire [7:0]  Imm;
         output reg [7:0]  M5;
 
         always @(*) begin
-        case (ALU2)
-            3'b000: M5 = OpB;
-            3'b001: M5 = 7'b01;
-            3'b010: M5 = Imm4;
-            3'b011: M5 = Imm5;
-            3'b100: M5 = Imm3; 
+        case (ALUSrcB)
+            2'b00: M5 = B;
+            2'b01: M5 = 8'd4;
+            2'b10: M5 = Imm;
             default: M5 = 8'bxxxxxxxx;
         endcase
     end
