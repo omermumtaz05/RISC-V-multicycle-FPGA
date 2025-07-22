f782a947471dd6ee7e9790d5f7a30cc63f8695d3
Muhammadahmed-1 <68020566+Muhammadahmed-1@users.noreply.github.com>
2025-07-22 00:35:00 -0400

    Update top.v

--- top.v
+++ top.v
@@ -3,7 +3,7 @@ module top_module(clock, reset);
       input wire clock;
       input wire reset;
 
-        // ALL CONTROL SIGNALS:
+    // CONTROL SIGNALS:
 
         //control signals (1-bit)
         wire IorD, ALUSrcA, MemtoReg, PCSource, PCWriteCond, PCWrite, 
@@ -28,90 +28,39 @@ module top_module(clock, reset);
         wire [31:0] M1, M2, M3, M4, M5;
     
 
-        assign PCcontol = PCWrite | (PCWriteCond & zero);
+        assign PCcontrol = PCWrite | (PCWriteCond & zero);
+        FSM fsm_inst (
+            clock, reset, IR_out[6:0]
+            ,regWrite, ALUSrcA, memRead, memWrite,MemtoReg, IorD, IRWrite, PCWrite, PCWriteCond,PCSource,
+            ALUOp,ALUSrcB
+        );
+        register registers_INST (IR_out[19:15], IR_out[24:20], IR_out[11:7], M3, regWrite, clock, regReadData1, regReadData2);
+        A A_INST (regReadData1,clock, reset, A);
+        B B_INST (regReadData2, clock, reset, B);
+        MDR MDR_INST (MDR, clock, reset, M3); // M3 is the output of the mux for MemtoReg
+        imm_gen imm_gen_INST (IR_out, Imm3); // Imm3 is the immediate value generated from the instruction
+       ALUControl ALUControl_INST (
+    IR_out[31:25], IR_out[14:12], ALUOp, reset, ALUControlLine);
 
-        // Instantiation the muxes
+         MDR MDR_INST (memData, reset, clock, MDR);
+         ALUOut ALUOut_INST (ALUResult, clock, reset, ALUOut); 
+
+
+        // Instantiation of muxes
         mem_mux MEM_INST (IorD, PC, ALUOut, M1);
         A_mux A_mux_INST (ALUSrcA, A, PC, M2);
         MDR_mux MDR_mux_INST (MemtoReg, MDR, ALUOut, M3);
         PC_mux pc_mux_inst (PCSource, ALUOut, ALUResult, M4);
         B_mux b_mux_inst (ALUSrcB, B, Imm3, M5);
 
-        
-        //functional units and temp regs datapath
+        ALU ALU_INST ( M2, M5, ALUControlLine, zero, ALUResult);
 
         ProgramCounter PC_INST (PCcontrol, M4, clock, reset, PC);  
-
-       /*  ProgramCounter(
-  input PCWrite,
-  input [31:0] next_pc,
-  input clk,
-  input reset,
-  output reg [31:0] pc)*/
-
-
-      /* module memory(
-    input [31:0]address,
-    input [31:0]writeData,
-    input memRead,
-    input memWrite,
-    input clk,
-    input reset,
-    output reg [31:0] memData
-);*/
-
-        memory memory_INST (M1, B, memRead, memWrite, clock, reset, memData); // done
-
-
-
-      /*  module IR(
-    input [31:0] MemData,
-    input IRWrite,
-    input clk,
-	input reset,
-  
-    output reg [31:0] inst
-
-);*/
-
+        memory memory_INST (M1, B, memRead, memWrite, clock, reset, memData); 
 
         IR IR_INST(memData, IRWrite, clock, reset, IR_out);
-
-
-    /*    module register(
-    input [4:0] readReg1,
-    input [4:0] readReg2,
-    input [4:0] writeReg,
-    input [31:0] writeData,
-    input regWrite,
-    input clk,
-  	input reset,
-
-    output reg [31:0] readData1,
-    output reg [31:0] readData2
-);*/
-
-        register registers_INST (IR_out[19:15], IR_out[24:20], IR_out[11:7], M3, regWrite, clock, reset, regReadData1, regReadData2);
-
-        ALU ALU_INST (M2, M5, ALUContrlLine, zero, ALUResult);
-
-
-
-
-        PC PC_INST (clock, reset, M4, PC);
-
-        control_unit control_unit_INST (clock, reset, PC, ALUResult, MDR,
-            IorD, ALUSrcA, MemtoReg, PCSource, ALUSrcB);
-
-      //  instruction_memory instruction_memory_INST (PC, M1);  
-
-       // data_memory data_memory_INST (clock, reset, ALUOut, MDR, M1);
-
-        instruction_decoder instruction_decoder_INST (M1, A, B, Imm3);
-
-        ALUControl ALU_control_INST (ALUResult, ALUSrcA, ALUSrcB);
-
         
 endmodule
  
- 
+
+
