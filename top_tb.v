0b6f2f67e5674219a4c840d7fed7492723247a1d
omermumtaz05 <omermumtaz05@gmail.com>
2025-07-26 18:40:56 +0400

    Create top_tb.v

---
+++ top_tb.v
@@ -0,0 +1,45 @@
+module top_tb();
+
+reg clock;
+reg reset;
+
+
+top_module uut(
+	.clock(clock),
+	.reset(reset)
+);
+
+
+
+  initial clock = 0;
+  always #5 clock = ~clock;  // Toggle every 5 ns ? 10 ns period = 100 MHz
+
+  // Test sequence
+  initial begin
+    
+    // Initialize inputs
+    clock = 0; reset = 1; 
+    
+    #10 reset = 0;
+    
+    #10
+    
+    
+    #10 
+    
+    
+    #10 
+    
+    
+    #10 
+    #10 
+	#10
+#10
+
+    #10 $stop;
+    
+    
+   
+  end
+
+endmodule
