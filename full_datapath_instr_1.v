e9a3b6f59348295a8b7de72e97b2b35416a4bbf0
omermumtaz05 <omermumtaz05@gmail.com>
2025-07-31 20:19:13 +0400

    Update full_datapath_instr_1.v
    made PC only be addresses in instruction memory range

--- full_datapath_instr_1.v
+++ full_datapath_instr_1.v
@@ -32,14 +32,19 @@ module ProgramCounter(
   input clk,
   input reset,
   output reg [31:0] pc);
-  
+
+
 
   always @ (posedge clk)
     if(reset)
       pc <= 32'b0;
     else if(PCWrite)
-      pc <= next_pc;
-    
+	begin
+      pc <= next_pc & 32'h0000007F;
+	
+     
+     
+    end
   
       
 endmodule
