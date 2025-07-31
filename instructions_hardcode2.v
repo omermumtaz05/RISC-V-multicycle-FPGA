b5c66b1934af62f070567c273aa4e12cf51a827a
omermumtaz05 <omermumtaz05@gmail.com>
2025-07-31 15:20:42 +0400

    Update instructions_hardcode2.v
    got rid of previous instructions in hardcoding

--- instructions_hardcode2.v
+++ instructions_hardcode2.v
@@ -36,43 +36,6 @@ module memory(
 	data[14] = 8'h00;
 	data[15] = 8'hfe;
 
-	//beq x3, x11, 8
-	data[16] = 8'h63;
-	data[17] = 8'h84;
-	data[18] = 8'hb1;
-	data[19] = 8'h00;
-	    
-	// garbage address
-	data[20] = 8'hxx;
-	data[21] = 8'hxx;
-	data[22] = 8'hxx;
-	data[23] = 8'hxx;
-	    
-	// garbage address
-	data[24] = 8'hxx;
-	data[25] = 8'hxx;
-	data[26] = 8'hxx;
-	data[27] = 8'hxx;
-	
-	// and x13, x8, x3
-	data[28] = 8'hb3;
-	data[29] = 8'h76;
-	data[30] = 8'h34;
-	data[31] = 8'h00;
-	    
-	// or x14, x8, x3
-	data[32] = 8'h33;
-	data[33] = 8'h67;
-	data[34] = 8'h34;
-	data[35] = 8'h00;
-	    
-	// sw x3, 150(x0)
-	data[36] = 8'h23;
-	data[37] = 8'h2b;
-	data[38] = 8'h30;
-	data[39] = 8'h08;
-	    
-	data[140] = 8'd82;
     end
 
 
@@ -110,45 +73,7 @@ module memory(
 	       data[14] <= 8'h00;
 	       data[15] <= 8'hfe;
 	       
-		//beq x3, x11, 8
-	       data[16] <= 8'h63;
-	       data[17] <= 8'h84;
-	       data[18] <= 8'hb1;
-	       data[19] <= 8'h00;
-	    
-		// garbage address
-	       data[20] <= 8'hxx;
-	       data[21] <= 8'hxx;
-	       data[22] <= 8'hxx;
-	       data[23] <= 8'hxx;
-	    
-		// garbage address
-	       data[24] <= 8'hxx;
-	       data[25] <= 8'hxx;
-	       data[26] <= 8'hxx;
-	       data[27] <= 8'hxx;
-	
-		// and x13, x8, x3
-	       data[28] <= 8'hb3;
-	       data[29] <= 8'h76;
-	       data[30] <= 8'h34;
-	       data[31] <= 8'h00;
-	    
-		// or x14, x8, x3
-	       data[32] <= 8'h33;
-	       data[33] <= 8'h67;
-	       data[34] <= 8'h34;
-	       data[35] <= 8'h00;
-	       
-	       	// sw x3, 150(x0)
-	       data[36] <= 8'h23;
-	       data[37] <= 8'h2b;
-	       data[38] <= 8'h30;
-	       data[39] <= 8'h08;
-	    
-	    
-	
-		data[140] <= 8'd82;
+
        end
 
        else if(memWrite)
